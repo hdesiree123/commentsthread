1e54a4c1a7d1d06d15daf7dfdeae077de7104ebe
hdesiree123 <86996846+hdesiree123@users.noreply.github.com>
2023-04-04 16:30:30 -0500

    adding all files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,127 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect, useRef } from 'react'
+//import { Button, Comment, Form } from 'semantic-ui-react'
+//import { Stack, Form, Button, Alert, Nav, Navbar, Container, NavDropdown, Offcanvas } from "react-bootstrap"
+
 
 function App() {
+const [name, setName] = useState ("");
+const [names, setNames] = useState ([]);
+const [comment, setComment] = useState ("");
+const [comments, setComments] = useState ([]);
+
+const [state, setState] = useState({
+  name: "",
+  comment: ""
+});
+
+const onChangeHandler = (e) => {
+  setComment(e.target.value);
+};
+
+const onChangeHandler2 = (e) => {
+  setName(e.target.value);
+};
+
+
+
+const onClickHandler = () => {
+  setNames((names) => [...names, name]);
+  setComments((comments) => [...comments, comment]);
+
+}
+
+const myList = names.map((item) => <p>{item}</p>)
+
+const onClickHandler2 = () => {
+  // <div>
+  //     <label> Enter comment:
+  //     <br></br>
+  //     <textarea value={comment} onChange={onChangeHandler}/>
+  //     <br></br>
+  //    </label>
+  //    <button onClick={onClickHandler}>Submit</button>
+  // </div>
+alert("Hello there!");
+<div>
+  <textarea>Some text here..</textarea>
+</div>
+
+
+}
+
+
+// const onReplyClick = () => {
+//   console.log("Hello");
+// //   <div>
+// // <fieldset>
+// //   <label> Enter comment:
+// //       <br></br>
+// //       <textarea value={comment} onChange={onChangeHandler}/>
+// //       <br></br>
+// //      </label>
+// //      <button onClick={onClickHandler}>Submit</button>
+// //      </fieldset>
+// //      </div>
+// }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    //take out main container?
+    <div className="main-container">
+     
+      <div>
+        <h1>Welcome Guest!</h1>   
+        <h2> This is the comment section. Please make a comment.</h2>
+        <h2> After making your first comment you can either reply, edit, or 
+            delete your comment.
+        </h2>
+      
+      <div className='wrapper'>
+        <fieldset>
+      <label> Enter name:
+      <br></br>
+      <input type="text" value={name} onChange={onChangeHandler2} required/>
+      {/* <input type="text" value={name} onChange={onChangeHandler}/> */}
+      {/* <textarea value={comment} onChange={onChangeHandler}/> */}
+      <br></br>
+     </label>
+
+     <label> Enter comment:
+      <br></br>
+      <textarea value={comment} onChange={onChangeHandler}/>
+      <br></br>
+     </label>
+     <button onClick={onClickHandler}>Submit</button>
+     </fieldset>
+     </div>
+      
+      
+     {/* {names.map((input) => (
+        <div> <p> Name: {input}</p></div>
+      ))}   */}
+      {/* comments will appear below */}
+        {comments.map((text2) => (
+         
+        <div className='wrapper'>
+        <fieldset>
+          <p>
+            
+            Name: {myList}
+          </p>
+        <p> Comment: {text2} </p> 
+        <button onClick={onClickHandler2}>Reply</button>
+        <t> </t>
+        <button>Edit</button>
+        <t> </t>
+        <button>Delete</button>
+        </fieldset>
+        
+        </div>
+
+      ))}
+
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,115 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { useState} from 'react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+root.render (
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// function MyForm() {
+//   const [name, setName] = useState("");
+//   const [comment, setComment] = useState("");
+//   const [textarea, setTextarea] = useState("");
+
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+
+//     // document.write(`User: ${name}`)
+//     // document.write(`Comment: ${comment}`)
+//     // document.getElementById('name').innerHTML;
+//     // document.getElementById('comment').innerHTML;
+
+//     //for memory purposes
+//     console.log({name});
+//     console.log({comment});
+//   }
+
+
+//   return (
+//     <div className="wrapper"> 
+//     <form onSubmit={handleSubmit}>
+//     <h1>Welcome Guest!</h1>   
+//     <h2> This is the comment section. Please make a comment.</h2>
+//     <h2> After making your first comment you can either reply, edit, or 
+//     delete your comment.
+//     </h2>
+//     <fieldset>
+//       <label>Enter your name:
+//       <br/>
+//         <t> </t>
+//         <input
+//           type="text" 
+//           value={name}
+//           onChange={(e) => setName(e.target.value)}
+//         />
+//       </label>
+//       <br></br>
+//       <br></br>
+//       <label>Enter your comment:
+//       <br/>
+//          {/* <input
+//           type="text" 
+//           value={comment}
+//           onChange={(e) => setComment(e.target.value)}
+//         />  */}
+//         <textarea value={comment} onChange={(e) => setComment(e.target.value)}/>
+//       </label>
+
+//       <br/>
+//       </fieldset>
+//        <input type="submit" /> 
+//     </form>
+//     </div>
+//   )
+// }
+
+// // const myFirstElement = (
+// //   <form id='details'>
+// // <div>
+// //   <h1>Welcome Guest!</h1>
+// //   <h2> This is the comment section. Please make a comment.</h2>
+// //   <h2> After making your first comment you can either reply, edit, or 
+// //     delete your comment.
+// //   </h2>
+// // <input type="text" placeholder='Name' id='Name'/>
+// // <br></br>
+// // <input type="text" placeholder='Comment' id='Comment'/>
+// // <br></br>
+// // <input type="button" value="Submit Comment" />
+// // {/* <input type="text" placeholder="Enter something..." id="inputData" />
+// //   <input type="submit" />
+// // <h1 id="valueHolder"></h1> */}
+// // </div>
+
+// // </form>
+
+
+
+
+
+// //  );
+
+
+
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<MyForm />);
+// // root.render(firstInput);
+// // root.render(
+// //   <React.StrictMode>
+// //     <App />
+// //   </React.StrictMode>
+// // );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
